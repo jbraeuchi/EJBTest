6ee21b05668d693c29000ae1aef6e5591bbe740e
jakob braeuchi <jbraeuchi@gmx.ch>
2016-10-02 12:12:37 +0200

    lookup

--- src/test/java/ch/brj/ejb/CallEJB.java
+++ src/test/java/ch/brj/ejb/CallEJB.java
@@ -20,15 +20,30 @@ public class CallEJB {
     //
     //  For stateful beans:
     // ejb:<app-name>/<module-name>/<distinct-name>/<bean-name>!<fully-qualified-classname-of-the-remote-interface>?stateful
+    //
+    // see https://blog.akquinet.de/2014/09/26/jboss-eap-wildfly-three-ways-to-invoke-remote-ejbs/
 
     @Test
-    public void test() throws Exception {
+    public void test_ejbNaming() throws Exception {
+        final Properties env = new Properties();
+        env.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
+
+        String name1 = "ejb:EJBTest/web/StatelessSessionRemoteBean!ch.brj.ejb.StatelessSessionRemote";
+
+        Context ctx = new InitialContext(env);
+        StatelessSessionRemote bean = (StatelessSessionRemote) ctx.lookup(name1);
+        String message = bean.greeting("Client");
+        System.out.println(message);
+    }
+
+    @Test
+    public void test_remoteNaming() throws Exception {
         final Properties env = new Properties();
         env.put(Context.INITIAL_CONTEXT_FACTORY, "org.jboss.naming.remote.client.InitialContextFactory");
         env.put(Context.PROVIDER_URL, "http-remoting://localhost:8080");
         env.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
 
-        String name1 = "ejb:EJBTest/web/StatelessSessionRemoteBean!ch.brj.ejb.StatelessSessionRemote";
+        String name1 = "/EJBTest/web/StatelessSessionRemoteBean!ch.brj.ejb.StatelessSessionRemote";
 
         Context ctx = new InitialContext(env);
         StatelessSessionRemote bean = (StatelessSessionRemote) ctx.lookup(name1);
